709cc3a72dcbb4b03de61aba8d52d7083a3fdc8f
Chum <ravichum@gmail.com>
2020-10-07 00:46:30 -0700

    Finished HW Commit

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,8 +1,27 @@
+
+
 public class Problem1 {
 
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
-        // Add your solution
-        return -1; // Place holder
+
+        int firstIndex = 0;
+        int lastIndex = data.length - 1;
+        int elementPos = -1;
+
+        //If the first index is less then last index run code
+        while (firstIndex <= lastIndex) {
+
+            int midIndex = (firstIndex + lastIndex) / 2;
+            if (target == data[midIndex]) {//If the target equals middle quit.
+                elementPos = midIndex;
+                break;
+            } else if (target < data[midIndex]) {
+                lastIndex = midIndex - 1;
+            } else if (target > data[midIndex]) {
+                firstIndex = midIndex + 1;
+            }
+        }
+        return elementPos;
     }
 }

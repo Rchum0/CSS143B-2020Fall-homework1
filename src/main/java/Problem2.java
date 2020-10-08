6edc0c4058b609f1d6b74a4763da105a410e279a
Chum <ravichum@gmail.com>
2020-10-07 18:17:37 -0700

    Bubble Sort finished code

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -2,6 +2,21 @@ public class Problem2 {
 
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
-        // Add your solution
+
+        boolean sorted = false;
+        int temp;
+
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < data.length - 1; i++) {
+                if (data[i] > data[i + 1]) {
+                    temp = data[i];
+                    data[i] = data[i + 1];
+                    data[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
     }
 }
+
